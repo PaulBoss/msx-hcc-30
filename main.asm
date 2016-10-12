c72b01a6b0b051200b77825d34b1fdc6e9cc9168
Paul Bosselaar <paul@bosselaar.net>
2016-10-12 23:03:59 +0200

    Added tiles + moving sprites

--- main.asm
+++ main.asm
@@ -4,7 +4,7 @@
 		dw START,0,0,0,0,0,0
 		
 		INCLUDE "bios.asm"
-
+		
 SPRITEY: EQU 0
 SPRITEX: EQU 1
 SPRITEPAT: EQU 2
@@ -14,11 +14,11 @@ SPRITEC: EQU 3
 START:
 		CALL INITSCR
 		CALL INITINT
-		CALL INITCHARS
 		CALL INITSPRITES
 		
 		LD A,0
 		LD (COUNTER), A
+		LD (COUNTER2), A
 		LD (OLDJIF), A
 		LD HL, SINTAB32
 MAIN:			
@@ -31,7 +31,7 @@ MAIN:
 		
 		PUSH HL
 		
-		LD HL, TILEDATA.TILES
+		LD HL, BANK_PATTERN_0
 		LD DE, MOVINGCHAR
 		LD A, (COUNTER)
 		AND 7
@@ -64,6 +64,45 @@ NOXLOOP:
 		POP HL
 		
 NOCHANGE:
+		LD B,6
+		LD IX, SPRITEBUFFER
+		LD IY, SPRITEATTR
+		
+		PUSH HL
+		PUSH BC
+SPRITEMOVELOOP:
+		LD A, (IY + SPRITEX)
+		SUB (HL)
+		LD (IX + SPRITEX), A
+
+		PUSH BC
+		EX DE, HL
+		LD HL, SINTAB32
+		LD A, (COUNTER2)
+		LD C, A
+		LD A, (IY + SPRITEY)
+		LD B,0
+		
+		ADD HL, BC
+		LD B, (HL)
+		SRA B
+		SUB B
+		LD (IX + SPRITEY), A
+		EX DE, HL
+		POP BC
+		
+		
+		PUSH DE
+		LD DE, 4
+		ADD IX, DE
+		ADD IY, DE
+		POP DE
+		DJNZ SPRITEMOVELOOP
+
+		POP BC
+		POP HL
+
+
 		INC HL
 		; Did we reach the end of SINTAB32? Reset. TODO: Align sintab to 256 address boundary and only in L
 		LD A,128
@@ -75,6 +114,12 @@ ENDLOOP:
 		LD A, (COUNTER)
 		INC A
 		LD (COUNTER), A
+		
+		LD A, (COUNTER2)
+		INC A
+		INC A
+		LD (COUNTER2), A
+		
 		JP MAIN
 
 		
@@ -112,64 +157,34 @@ INITSCR:
 		
 		LD A,(RG0SAV + 1)	; Load VDP reg 1 FROM BIOS mirror
 		AND %11111100	; Reset bits 1 (SIZE) and 0 (MAG)
-		OR 1			; Set bit 0 (16x16 sprites, magnified)
+		OR 3			; Set bit 0 (16x16 sprites, magnified)
 		LD B,A		
 		LD C,1			; Set VDP reg 1
 		CALL WRTVDP
 		
 		LD DE,(GRPCGP) ; Fill tiles
-		LD BC,16
-		LD HL, TILEDATA.TILES
+		LD BC,6144
+		LD HL, BANK_PATTERN_0
 		CALL LDIRVM
-		
-		LD HL, (GRPCGP)
-		LD DE, 2048
-		ADD HL, DE
-		EX DE,HL
-		LD BC,16
-		LD HL, TILEDATA.TILES
-		CALL LDIRVM
-		
-		LD HL, (GRPCGP)
-		LD DE, 4096
-		ADD HL, DE
-		EX DE,HL
-		LD BC,16
-		LD HL, TILEDATA.TILES
-		CALL  LDIRVM
 		
 		LD DE,(GRPCOL)	; Fill color table
-		LD BC,16
-		LD HL, TILEDATA.COLORS
+		LD BC,6144
+		LD HL, BANK_COLOR_0
 		CALL LDIRVM
 
-		LD HL,(GRPCOL)	; Fill color table
-		LD DE, 2048
-		ADD HL, DE
-		EX DE, HL
-		LD BC,16
-		LD HL, TILEDATA.COLORS
-		CALL LDIRVM
-		
-		LD HL,(GRPCOL)	; Fill color table
-		LD DE, 4096
-		ADD HL, DE
-		EX DE, HL
-		LD BC,16
-		LD HL, TILEDATA.COLORS
-		CALL LDIRVM
-		
-		LD HL,(GRPNAM)		; Clear the screen
-		LD A,0
-		LD BC,786
-		CALL FILVRM
-	
 		; Copy moving char pattern to MOVINGCHAR
-		LD HL, TILEDATA.TILES
+		LD HL, BANK_PATTERN_0
 		LD DE, MOVINGCHAR
 		LD BC,8
 		LDIR
-	
+
+		LD HL, SCREEN 
+		LD DE,(GRPNAM)		; Init the screen data
+		LD BC, 768
+		LD HL, SCREEN
+		CALL LDIRVM
+		
+		
 		RET
 	
 INITINT:
@@ -195,23 +210,23 @@ NEWHK:
 MYINT:	; Code called from H.TIMI hook
 
 		; For timing, set the border color to blue
-		ld a,4
-		out (0x99),a
-		ld a,7+128
-		out (0x99),a
+		;ld a,4
+		;out (0x99),a
+		;ld a,7+128
+		;out (0x99),a
 		
 		; Copy "moving" background char to VDP
-		LD DE,(GRPCGP) ; Fill tiles
+		LD DE,(GRPCGP) ; 
 		LD BC,8
 		LD HL, MOVINGCHAR
 		CALL LDIRVM
 		
-		LD DE,(GRPCGP + 2048) ; Fill tiles
+		LD DE,(GRPCGP + 2048) ; Fill tiles part 1
 		LD BC,8
 		LD HL, MOVINGCHAR
 		CALL LDIRVM
 		
-		LD HL, (GRPCGP)
+		LD HL, (GRPCGP)		; Fill tiles part 2
 		LD DE, 2048
 		ADD HL, DE
 		EX DE,HL
@@ -219,7 +234,7 @@ MYINT:	; Code called from H.TIMI hook
 		LD HL, MOVINGCHAR
 		CALL LDIRVM
 		
-		LD HL, (GRPCGP)
+		LD HL, (GRPCGP)	;	Fill tiles part 2
 		LD DE, 4096
 		ADD HL, DE
 		EX DE,HL
@@ -229,107 +244,75 @@ MYINT:	; Code called from H.TIMI hook
 		
 		LD    DE,(GRPATR) ; Show the sprites
         LD    HL,SPRITEBUFFER
-        LD    BC,4
+        LD    BC,24
         CALL  LDIRVM
 		
 		; For timing, set the border color to black
-		ld a,0
-		out (0x99),a
-		ld a,7+128
-		out (0x99),a
+		;ld a,0
+		;out (0x99),a
+		;ld a,7+128
+		;out (0x99),a
 		
 		RET
 		
 INITSPRITES:
 		LD    DE,(GRPPAT) ; Fill Sprite attribute table
-		LD    BC, 32
-		LD    HL,CHARBUFFER+64*8
+		LD    BC, 256
+		LD    HL, SPRITES 
 		CALL  LDIRVM	
 		
-
-		
-		LD IX, SPRITEBUFFER
-		LD (IX+SPRITEY), 50
-		LD (IX+SPRITEX), 50
-		LD (IX+SPRITEPAT), 1
-		LD (IX+SPRITEC), 15
-		INC IX
-		INC IX
-		INC IX
-		INC IX
+		LD HL, SPRITEATTR
+		LD DE, SPRITEBUFFER
+		LD BC, 24
+		LDIR
 		
 		RET
 		
-; Copy char definition from ROM to RAM in current slot		
-INITCHARS:
-		LD BC, 2048
-		LD DE, CHARBUFFER
-		LD HL, (CGPNT+1)
-INITCHARLOOP:
-		PUSH BC
-		PUSH DE
-		LD A,(CGPNT)
-		CALL RDSLT
-		EI
-		POP DE
-		POP BC
-		LD (DE),A
-		INC DE
-		INC HL
-		DEC BC
-		LD A,B
-		OR C
-		JR NZ, INITCHARLOOP
-		RET
+				
+SPRITES:
+		; 3
+		DB 0xFF,0xFF,0xFF,0xFF,0x00,0x00,0xFF,0xFF
+		DB 0xFF,0xFF,0x00,0x00,0xFF,0xFF,0xFF,0xFF
+		DB 0xFC,0xFE,0xFF,0xFF,0x0F,0x0F,0xFF,0xFF
+		DB 0xFF,0xFF,0x0F,0x0F,0xFF,0xFF,0xFE,0xFC
+		; 0
+		DB 0x3F,0x7F,0xFF,0xFF,0xF0,0xF0,0xF0,0xF0
+		DB 0xF0,0xF0,0xF0,0xF0,0xFF,0xFF,0x7F,0x3F
+		DB 0xFC,0xFE,0xFF,0xFF,0x0F,0x0F,0x0F,0x0F
+		DB 0x0F,0x0F,0x0F,0x0F,0xFF,0xFF,0xFE,0xFC
+		; J
+		DB 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
+		DB 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
+		DB 0x0F,0x0F,0x0F,0x0F,0x0F,0x0F,0x0F,0x0F
+		DB 0x0F,0x0F,0x0F,0x0F,0xFF,0xFF,0xFE,0xFC
+		; A
+		DB 0x3F,0x7F,0xFF,0xFF,0xF0,0xF0,0xF0,0xFF
+		DB 0xFF,0xFF,0xFF,0xF0,0xF0,0xF0,0xF0,0xF0
+		DB 0xFC,0xFE,0xFF,0xFF,0x0F,0x0F,0x0F,0xFF
+		DB 0xFF,0xFF,0xFF,0x0F,0x0F,0x0F,0x0F,0x0F
+		; A
+		DB 0x3F,0x7F,0xFF,0xFF,0xF0,0xF0,0xF0,0xFF
+		DB 0xFF,0xFF,0xFF,0xF0,0xF0,0xF0,0xF0,0xF0
+		DB 0xFC,0xFE,0xFF,0xFF,0x0F,0x0F,0x0F,0xFF
+		DB 0xFF,0xFF,0xFF,0x0F,0x0F,0x0F,0x0F,0x0F
+		; R
+		DB 0xFF,0xFF,0xFF,0xFF,0xF0,0xF0,0xF0,0xFF
+		DB 0xFF,0xFF,0xFF,0xF0,0xF0,0xF0,0xF0,0xF0
+		DB 0xFC,0xFE,0xFF,0xFF,0x0F,0x0F,0x0F,0xFF
+		DB 0xFF,0xFE,0xFC,0x3E,0x1F,0x0F,0x0F,0x0F
 		
-		
-		
-MOVE:   DB 0,0,0,0,0,1,0,1,1,1,1,1,2,2,2,2,3,3,3,3,2,2,2,2,1,1,1,1,1,0,1,0
-		DB 0,0,0,0,0,7,0,7,7,7,7,7,6,6,6,6,5,5,5,5,6,6,6,6,7,7,7,7,7,0,7,0,255
-			
-SPRITE:
-		DB 10101010b
-		DB 01010101b
-		DB 10101010b
-		DB 01010101b
-		DB 10101010b
-		DB 01010101b
-		DB 10101010b
-		DB 01010101b
-		
-		DB 10101010b
-		DB 01010101b
-		DB 10101010b
-		DB 01010101b
-		DB 10101010b
-		DB 01010101b
-		DB 10101010b
-		DB 01010101b
-		
-		DB 10101010b
-		DB 01010101b
-		DB 10101010b
-		DB 01010101b
-		DB 10101010b
-		DB 01010101b
-		DB 10101010b
-		DB 01010101b
-		
-		DB 10101010b
-		DB 01010101b
-		DB 10101010b
-		DB 01010101b
-		DB 10101010b
-		DB 01010101b
-		DB 10101010b
-		DB 01010101b
 	
 SPRITEATTR:
-		DB 10,0,0,15
-		DB 10,32,0,15
-		DB 10,64,0,15
-		DB 10,96,0,15
+		DB 90,92,0,15
+		DB 90,128,4,15
+		
+		DB 130,48,8,15
+		DB 130,84,12,15
+		DB 130,120,16,15
+		DB 130,156,20,15
 
+
+		
 SINTAB32:
 		DB 0,1,2,2,3,4,5,5,6,7,8,9,9,10,11,12,12,13,14,14,15,16,16,17,18,18,19,20,20,21,21,22,23
 		DB 23,24,24,25,25,26,26,27,27,27,28,28,29,29,29,30,30,30,30,31,31,31,31,31,32,32,32,32,32,32,32,32
@@ -340,26 +323,17 @@ SINTAB32:
 		DB -32,-32,-32,-32,-32,-32,-32,-31,-31,-31,-31,-31,-30,-30,-30,-30,-29,-29,-29,-28,-28,-27,-27,-27,-26,-26,-25,-25,-24,-24,-23,-23
 		DB -22,-21,-21,-20,-20,-19,-18,-18,-17,-16,-16,-15,-14,-14,-13,-12,-12,-11,-10,-9,-9,-8,-7,-6,-5,-5,-4,-3,-2,-2,-1
 		DB 128
-SINTAB8:
-		DB 0,0,0,1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,3,3,4,4,4,4,4,4,4,5,5,5,5
- 		DB 5,5,5,5,6,6,6,6,6,6,6,6,6,6,6,6,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7
-		DB 7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,5
-		DB 5,5,5,4,4,4,4,4,4,4,3,3,3,3,3,3,3,2,2,2,2,2,2,1,1,1,1,1,1,0,0,0
-		DB 0,0,-1,-1,-1,-1,-1,-1,-2,-2,-2,-2,-2,-2,-3,-3,-3,-3,-3,-3,-3,-4,-4,-4,-4,-4,-4,-4,-5,-5,-5,-5
-		DB -5,-5,-5,-5,-6,-6,-6,-6,-6,-6,-6,-6,-6,-6,-6,-6,-7,-7,-7,-7,-7,-7,-7,-7,-7,-7,-7,-7,-7,-7,-7,-7
-		DB -7,-7,-7,-7,-7,-7,-7,-7,-7,-7,-7,-7,-7,-7,-7,-6,-6,-6,-6,-6,-6,-6,-6,-6,-6,-6,-6,-5,-5,-5,-5,-5
-		DB -5,-5,-5,-4,-4,-4,-4,-4,-4,-4,-3,-3,-3,-3,-3,-3,-3,-2,-2,-2,-2,-2,-2,-1,-1,-1,-1,-1,-1,0,0
-		DB 128
 		
-		INCLUDE "tiles.asm"		
-
-		DS 0x8000-$
+		INCLUDE "tiles2.asm"
+		INCLUDE "screen2.asm"
+		
+		DS 0xC000-$
 		org 0xC000
 MOVINGCHAR: DS VIRTUAL 8	
 OLDHK:	DS VIRTUAL 5
 OLDJIF:	DS VIRTUAL 2
 CHARPOSX: DS VIRTUAL 2
-SPRITEBUFFER: DS VIRTUAL 256
+SPRITEBUFFER: DS VIRTUAL 24
 COUNTER: DS VIRTUAL 1
-CHARBUFFER: DS VIRTUAL 2048
+COUNTER2: DS VIRTUAL 1
 
